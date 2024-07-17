384e7da372d4b2674919b6a955c76dbd12c8ab3c
Ahmed Abdelsalam Ragab <ahmed.ragab@nt-me.com>
2024-07-17 17:10:07 +0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./Components/header/header.component";
+import { SidebarComponent } from "./Components/sidebar/sidebar.component";
+import { HomeComponent } from "./Components/home/home.component";
+import { FooterComponent } from "./Components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, HomeComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'CourseDemo';
+  title:string = 'CourseDemo';
 }
