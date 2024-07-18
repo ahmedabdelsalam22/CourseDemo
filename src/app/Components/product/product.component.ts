4ee4ae3db9a7fe54cb746612e8192210455faf33
Ahmed Abdelsalam Ragab <ahmed.ragab@nt-me.com>
2024-07-18 17:01:32 +0300

    create product component and IProduct act as a model and display prouducts in product page

--- src/app/Components/product/product.component.ts
+++ src/app/Components/product/product.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { IProduct } from '../../Models/iproduct';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
 export class ProductComponent {
 
+  prdList:IProduct[];
+
+  constructor() {
+    this.prdList = [
+      {id:1, name: "IPhone 13", price:30000 , quantity:4 , imgUrl:'https://dummyimage.com/300x200/000/fff' , catId:1},
+      {id:2, name: "IPhone 6s", price:6000 , quantity:1 , imgUrl:'https://dummyimage.com/300x200/000/fff' , catId:1},
+      {id:3, name: "Symsung A51", price:10000 , quantity:2 , imgUrl:'https://dummyimage.com/300x200/000/fff' , catId:2},
+    ];
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,13 @@ import { SidebarComponent } from "./Components/sidebar/sidebar.component";
 import { HomeComponent } from "./Components/home/home.component";
 import { FooterComponent } from "./Components/footer/footer.component";
 import { FormsModule } from '@angular/forms';
+import { ProductComponent } from "./Components/product/product.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent, HomeComponent, FooterComponent, FormsModule],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, HomeComponent, FooterComponent, FormsModule, ProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
