356263b2c95277ec0f7f62f100da71ca276bcd28
Ahmed Abdelsalam Ragab <ahmed.ragab@nt-me.com>
2024-07-21 16:14:52 +0300

    build select category in orderMaster and bind the selectedCategoryId by ngModel in ts, then Display it in OrderMaster for testing

--- src/app/Components/order-master/order-master.component.ts
+++ src/app/Components/order-master/order-master.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { ProductComponent } from "../product/product.component";
+import { ICategory } from '../../Models/icategory';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-order-master',
   standalone: true,
-  imports: [ProductComponent],
+  imports: [ProductComponent, CommonModule, FormsModule],
   templateUrl: './order-master.component.html',
   styleUrl: './order-master.component.scss'
 })
 export class OrderMasterComponent {
+
+  catList:ICategory[];
+  selectedCateId:number=0;
+
+  constructor() {
+    this.catList = [
+      {id:1, name: "Pc" },
+      {id:2, name: "Mobile" },
+      {id:3, name: "Accessoires" },
+    ];
+  }
 
 }
