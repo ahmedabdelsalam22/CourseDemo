2a01fe9db0b6a2b98f31546d43b466bd625795ba
Ahmed Abdelsalam Ragab <ahmed.ragab@nt-me.com>
2024-07-22 10:29:22 +0300

    filter product by category id

--- src/app/Components/product/product.component.ts
+++ src/app/Components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { IProduct } from '../../Models/iproduct';
 import { CommonModule } from '@angular/common';
 
@@ -9,24 +9,39 @@ import { CommonModule } from '@angular/common';
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
-export class ProductComponent {
+export class ProductComponent implements OnChanges{
 
   prdList:IProduct[];
   @Input() CategoryId:number=0;
   totalPrice:number= 0;
 
+  prdListByCatId:IProduct[] = [];
+
   constructor() {
     this.prdList = [
-      {id:7, name: "IPhone 13", price:30000 , quantity:4 , imgUrl:'https://dummyimage.com/100x100/000/fff' , catId:1},
-      {id:15, name: "IPhone 6s", price:6000 , quantity:0 , imgUrl:'https://dummyimage.com/100x100/000/fff' , catId:1},
+      {id:7, name: "IPhone 13", price:30000 , quantity:4 , imgUrl:'https://dummyimage.com/100x100/000/fff' , catId:2},
+      {id:15, name: "IPhone 6s", price:6000 , quantity:0 , imgUrl:'https://dummyimage.com/100x100/000/fff' , catId:2},
       {id:20, name: "Symsung A51", price:10000 , quantity:2 , imgUrl:'https://dummyimage.com/100x100/000/fff' , catId:2},
+      {id:21, name: "Mac M1 2020", price:1000 , quantity:3 , imgUrl:'https://dummyimage.com/100x100/000/fff' , catId:1},
     ];
   }
+  ngOnChanges(changes: SimpleChanges): void {
+    if(this.CategoryId == 0)
+    {
+      this.prdListByCatId = this.prdList;
+    }
+    else
+    {
+      this.prdListByCatId = this.prdList.filter(x=>x.catId == this.CategoryId);
+    }
+  }
 
+  /* 
   productTrackedBy(index:number, item:IProduct)
   {
     return item.id;
   }
+  */
 
   updateOrderTotalPrice(count:number , price:number)
   {
