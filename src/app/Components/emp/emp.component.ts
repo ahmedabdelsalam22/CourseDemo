b9ae38057941af17bfa469f9a54b19eb7a67fe18
Ahmed Abdelsalam Ragab <ahmed.ragab@nt-me.com>
2024-07-23 16:14:30 +0300

    routing

--- src/app/Components/emp/emp.component.ts
+++ src/app/Components/emp/emp.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { EmpService } from '../../Services/emp.service';
 import { IEmployees } from '../../Models/iemployees';
 import { CommonModule } from '@angular/common';
+import { error } from 'console';
 
 @Component({
   selector: 'app-emp',
@@ -13,8 +14,17 @@ import { CommonModule } from '@angular/common';
 export class EmpComponent {
 
   empsList:IEmployees[] = [];
+  errorMsg:any;
 
   constructor(private empService:EmpService) {
-    empService.getAllEmps().subscribe(emps=> this.empsList = emps);
+    empService.getAllEmps().subscribe(
+      emps=> 
+        {
+          this.empsList = emps
+        },
+      error=>
+        {
+         this.errorMsg = error
+        });
   }
 }
